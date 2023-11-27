52f77d4629b5aac590d6250bf8460b3de6a2e3b6
40758 <40758@DESKTOP-U6HJMHG>
2023-11-27 18:19:55 +0200

    Laborator 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,24 @@
-﻿namespace Sinca_Teodora_Lab7;
+﻿using System;
+using Sinca_Teodora_Lab7.Data;
+using System.IO;
+namespace Sinca_Teodora_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+
+    public static ShoppingListDatabase Database 
+	{
+		get
+		{
+			if (database == null)
+			{ 
+				database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+			}
+			return database; 
+		} 
+	}
+    public App()
 	{
 		InitializeComponent();
 
